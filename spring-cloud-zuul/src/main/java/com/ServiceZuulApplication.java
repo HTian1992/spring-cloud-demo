f722c0b6133318c367590195e528067af5e879f0
浩天天sKy <641801732@qq.com>
2018-08-28 15:42:24 +0800

    add zuul-server

---
+++ spring-cloud-zuul/src/main/java/com/ServiceZuulApplication.java
@@ -0,0 +1,17 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+//启用服务路由功能
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class ServiceZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceZuulApplication.class, args);
+    }
+}
